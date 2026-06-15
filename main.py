448025d7c16610abf269c95ada14ad6ee7e9d600
Jonathan Menjivar <ctejam22@gmail.com>
2026-06-14 22:19:47 -0600

    prueba checkout1

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-print('Inventario virtual /n calderas leon')
-#login, inicio de sesion, base de datos de inicio de sesion con tupla modificable.
-log = 0
+print("""
+=========================
+ INVENTARIO VIRTUAL
+      
+      
+ CALDERAS LEÓN
+=========================
+""")
 log= int(input ('que desea hacer? registrarse (1) o iniciar sesion (2)'))
 if log == 1:
-    correo = input('escriba su nuevo correo empresarial')
-    contrase;a= input('escriba su contrase;a')
+    correo = input('escriba su nuevo correo empresarial: ')
+    contrase;a= input('escriba su contrase;a: ')
 else:
-    correo = input('escriba su correo empresarial')
-    contrase;a= input('escriba su contrase;a')
+    correo = input('escriba su correo empresarial: ')
+    contrase;a= input('escriba su contrase;a: ')
