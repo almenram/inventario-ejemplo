8023101b8cf5c39c63387cc33ef0e87069c5f437
Jonathan Menjivar <ctejam22@gmail.com>
2026-06-14 22:15:31 -0600

    primer print

---
+++ main.py
@@ -0,0 +1,10 @@
+print('Inventario virtual /n calderas leon')
+#login, inicio de sesion, base de datos de inicio de sesion con tupla modificable.
+log = 0
+log= int(input ('que desea hacer? registrarse (1) o iniciar sesion (2)'))
+if log == 1:
+    correo = input('escriba su nuevo correo empresarial')
+    contrase;a= input('escriba su contrase;a')
+else:
+    correo = input('escriba su correo empresarial')
+    contrase;a= input('escriba su contrase;a')
