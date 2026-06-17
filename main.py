e115215a8409be3516905926716f178805104770
Jonathan Menjivar <ctejam22@gmail.com>
2026-06-16 21:46:31 -0600

    login terminado

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
+import json
 print("""
 =========================
  INVENTARIO VIRTUAL
       
-      
- CALDERAS LEÓN
 =========================
 """)
+def mostrar_menu():
+    opcion=int(input("bienvenido" \
+    "?que desea hacer?" \
+    "(inicio de sesion (1)" \
+    "registrarse (2)\n"))
+    return opcion
+
+    
 
 correo_guardado = ""
 contrasena_guardada = ""
+while True:
+    opcion=mostrar_menu()
+    if opcion==1:
+        correo_guardado=input("ingrese su correo guardado:-")
+        contrasena_guardada=input("ingrese su contrase;a guardada:-")
+        with open("usuarios.json", "r") as archivo:
+           datos = json.load(archivo)
+        print(datos)
 
 
-
-import json
-
-with open("usuarios.json") as archivo:
-    datos = json.load(archivo)
-
-print(datos)
+        break
+   
+    else:
+        correo_guardado=input( "ingrese su correo empresaria:-")
+        contrasena_guardada=input('ingrese su contrase;a guardada:-')
+        datos = {}
+        datos["correo"] = correo_guardado
+        datos["contrasena"] = contrasena_guardada
+        with open("usuarios.json", "w") as archivo:
+            json.dump(datos, archivo, indent=4)
